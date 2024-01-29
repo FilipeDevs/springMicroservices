36cccf4c62fb7c28849431ed644052dfe3dbc9e0
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-01-29 21:56:39 +0100

    Add Discovery Server-Eureka + setup Eureka Clients on each service and add load balancing on OrderService

--- microservicesParent/order-service/src/main/java/com/filipeDevs/orderservice/config/WebClientConfig.java
+++ microservicesParent/order-service/src/main/java/com/filipeDevs/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.filipeDevs.orderservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,9 +8,15 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    // Add Load Balancing on this Eureka Client (since we will have multiple instances of the different microservices)
+
+    // The logic of Load Balancer is part of the client itself, and it carries the list of
+    // servers and determines to which server a particular request must be directed based
+    // on some algorithm. (https://www.geeksforgeeks.org/spring-cloud-client-side-load-balancer/)
     @Bean
-    public WebClient webClient() {
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 
 
