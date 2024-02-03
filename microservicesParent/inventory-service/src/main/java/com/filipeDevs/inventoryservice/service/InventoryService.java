91ba278b6cc8465b0cf4676afe276d8f45400de7
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-02-03 20:44:35 +0100

    Add time limiter

--- microservicesParent/inventory-service/src/main/java/com/filipeDevs/inventoryservice/service/InventoryService.java
+++ microservicesParent/inventory-service/src/main/java/com/filipeDevs/inventoryservice/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.filipeDevs.inventoryservice.service;
 import com.filipeDevs.inventoryservice.dto.InventoryResponse;
 import com.filipeDevs.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
+    @SneakyThrows
     public List<InventoryResponse> isInStock(List<String> scanCode) {
+        log.info("Wait started");
+        Thread.sleep(10000);
+        log.info("Wait ended");
         // Find all inventories from the list of the product's scanCode
         // and check if the inventory of the product still has stock
         return inventoryRepository.findByScanCodeIn(scanCode)
--- microservicesParent/order-service/src/main/java/com/filipeDevs/orderservice/controller/OrderController.java
+++ microservicesParent/order-service/src/main/java/com/filipeDevs/orderservice/controller/OrderController.java
@@ -3,9 +3,12 @@ package com.filipeDevs.orderservice.controller;
 import com.filipeDevs.orderservice.dto.OrderRequest;
 import com.filipeDevs.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -17,12 +20,13 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest) {
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest) {
+        // Async execution so timeout of the circuit breaker works
+        return CompletableFuture.supplyAsync(() -> orderService.placeOrder(orderRequest));
     }
 
-    public String fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
-        return "Oops ! Something went wrong, please try again later !";
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        return  CompletableFuture.supplyAsync(() -> "Oops ! Something went wrong, please try again later !") ;
     }
 }
